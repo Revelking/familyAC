2a4dec92322b9ff742f4783228540b2a86447d69
hp <983327806@qq.com>
2016-04-17 13:54:23 +0800

    注册

--- AdventureClub/AdventureClub/SignUpViewController.m
+++ AdventureClub/AdventureClub/SignUpViewController.m
@@ -36,6 +36,60 @@
 
 //当用户点击确认注册按钮后调用
 - (IBAction)signUpAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    NSString *username = _usernameTF.text;
+    NSString *email = _emailTF.text;
+    NSString *phone=_phoneTF.text;
+    NSString *password = _passwordTF.text;
+    NSString *confirmPassword = _confirmPasswordTF.text;
+    
+    if (username.length == 0 || email.length == 0 || password.length == 0 || confirmPassword.length == 0) {
+        [Utilities popUpAlertViewWithMsg:@"内容为空，请输入完整" andTitle:nil onView:self];
+        return;
+    }
+    if (![password isEqualToString:confirmPassword]) {
+        [Utilities popUpAlertViewWithMsg:@"密码不相同，请重新输入" andTitle:nil onView:self];
+        return;
+    }
+     PFUser *user = [PFUser user];
+    user.username = username;
+    user.email = email;
+    user.password = password;
+    user[@"phone"]=phone;
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error){
+        if (!error) {
+           
+            UIAlertController *alertView = [UIAlertController alertControllerWithTitle:@"提示" message:@"注册成功" preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction *ac=[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                [self .navigationController popViewControllerAnimated:YES];
+            } ];
+           
+            [alertView addAction:ac];
+           
+            [self  presentViewController:alertView animated:YES completion:nil];
+
+        }else {
+            switch (error.code) {
+                case 202:
+                    [Utilities popUpAlertViewWithMsg:@"该用户名已被使用" andTitle:nil onView:self];
+                    break;
+                case 203:
+                    [Utilities popUpAlertViewWithMsg:@"该电子邮箱已被使用" andTitle:nil onView:self];
+                    break;
+                case 100:
+                    [Utilities popUpAlertViewWithMsg:@"网络不给力，请稍后再试" andTitle:nil onView:self];
+                    break;
+                case 125:
+                    [Utilities popUpAlertViewWithMsg:@"该电子邮箱不存在" andTitle:nil onView:self];
+                    break;
+                default:
+                    [Utilities popUpAlertViewWithMsg:@"服务器正在维护，请稍后再试" andTitle:nil onView:self];
+                    break;
+            }
+        
+        }
+    }];
+    
+    
 }
 
 //当键盘右下角的确认按钮被按时收键盘
