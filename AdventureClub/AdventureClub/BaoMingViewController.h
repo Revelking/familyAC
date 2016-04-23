e78d150879ba8c9a64fe534eef3b7d4fa62e2823
wang <2665630030@qq.com>
2016-04-23 14:44:29 +0800

    报名详情页

---
+++ AdventureClub/AdventureClub/BaoMingViewController.h
@@ -0,0 +1,39 @@
+//
+//  BaoMingViewController.h
+//  AdventureClub
+//
+//  Created by wang on 16/4/23.
+//  Copyright © 2016年 Family. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaoMingViewController : UIViewController
+//头像
+@property (weak, nonatomic) IBOutlet UIImageView *touXiang;
+//用户名
+@property (weak, nonatomic) IBOutlet UILabel *name;
+//活动图片
+@property (weak, nonatomic) IBOutlet UIImageView *imageTF;
+//活动名称
+@property (weak, nonatomic) IBOutlet UILabel *nameTF;
+//活动内容
+@property (weak, nonatomic) IBOutlet UILabel *contentTF;
+//活动限定人数
+@property (weak, nonatomic) IBOutlet UILabel *detailsTF;
+//开始时间
+@property (weak, nonatomic) IBOutlet UILabel *startTF;
+//结束时间
+@property (weak, nonatomic) IBOutlet UILabel *timeTF;
+
+//活动地点
+@property (weak, nonatomic) IBOutlet UILabel *addressTF;
+//注意事项
+@property (weak, nonatomic) IBOutlet UILabel *mattersTF;
+//活动电话
+@property (weak, nonatomic) IBOutlet UILabel *phoneTF;
+//此方法实现可打电话
+- (IBAction)phoneAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)baoMingAction:(UIBarButtonItem *)sender;
+
+@end
---
+++ AdventureClub/AdventureClub/BaoMingViewController.m
@@ -0,0 +1,42 @@
+//
+//  BaoMingViewController.m
+//  AdventureClub
+//
+//  Created by wang on 16/4/23.
+//  Copyright © 2016年 Family. All rights reserved.
+//
+
+#import "BaoMingViewController.h"
+
+@interface BaoMingViewController ()
+
+@end
+
+@implementation BaoMingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)phoneAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+
+- (IBAction)baoMingAction:(UIBarButtonItem *)sender {
+}
+@end
